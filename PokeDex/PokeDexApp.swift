6c1382330a21f2834954e0f0e7b5f57d48b74b9f
Tio Irawan <sattanationmail@gmail.com>
2024-06-25 11:33:39 +0700

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Tio Irawan on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
